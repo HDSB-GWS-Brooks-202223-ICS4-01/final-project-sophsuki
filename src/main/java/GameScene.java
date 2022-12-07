6ba3eb08221ca3de306af8f76160bc0c51a6230b
suukkii <83240082+suukkii@users.noreply.github.com>
2022-12-07 09:32:40 -0500

    Fixed errors in GameScene
    - changed textarea to label
    added font, background, colour

--- src/main/java/GameScene.java
+++ src/main/java/GameScene.java
@@ -1,14 +1,22 @@
 import javafx.geometry.Pos;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
 public class GameScene {
     
-    public VBox basicLayout (TextArea text, VBox box){
-        box.setAlignment(Pos.CENTER_LEFT);
-        text.setFont(new Font("CC Overbyte", 30));
-        box.getChildren().addAll(text, box);
-        return box;
+    public VBox basicLayout (Label text, VBox box){
+        VBox basic = new VBox();
+
+        basic.setStyle("-fx-background: black;");
+        basic.setAlignment(Pos.CENTER_LEFT);
+        
+        text.setTextFill(javafx.scene.paint.Color.RED);
+        text.setFont(new Font("Courier New", 30));
+
+        basic.getChildren().addAll(text, box);
+        
+        return basic;
     }
 }
