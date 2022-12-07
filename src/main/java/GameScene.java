3e503d0198318920198353feb908bf6d69b35518
soap123b <99285210+Soph055@users.noreply.github.com>
2022-12-07 17:41:29 -0500

    FIxed font, added new method to GameScene class
    Added new font file, and created .css file for font. Added style method and basic method in GameScene class.

--- src/main/java/GameScene.java
+++ src/main/java/GameScene.java
@@ -5,19 +5,33 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 
 public class GameScene {
-    
-    public VBox basicLayout (Label text, VBox box){
-        VBox basic = new VBox();
 
-        basic.setStyle("-fx-background: black;");
-        basic.setAlignment(Pos.CENTER_LEFT);
-        
-        text.setTextFill(javafx.scene.paint.Color.RED);
-        //text.setFont(new Font("Courier New", 30));
-        text.getStylesheets().add(getClass().getResource("/fontstyle.css").toExternalForm());
+    // public VBox basicLayout (Label text, VBox box){
+    // VBox basic = new VBox();
 
-        basic.getChildren().addAll(text, box);
-        
-        return basic;
+    // basic.setStyle("-fx-background: black;");
+    // basic.setAlignment(Pos.CENTER_LEFT);
+
+    // text.setTextFill(javafx.scene.paint.Color.WHITESMOKE);
+    // text.getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
+
+    // basic.getChildren().addAll(text, box);
+
+    // return basic;
+    // }
+
+    public VBox basic( VBox box) { // use this instead of function above explain to jelena tmrw in class.
+        box.setStyle("-fx-background: black;");
+        box.setAlignment(Pos.CENTER);
+        return box;
+    }
+
+    public Label styleText (Label text, int x, int y){
+        text.setTextFill(javafx.scene.paint.Color.WHITESMOKE);
+        text.getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
+        text.setTranslateX(x);
+        text.setTranslateY(y);
+
+        return text;
     }
 }
