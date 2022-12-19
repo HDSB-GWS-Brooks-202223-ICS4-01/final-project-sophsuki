2415a9584477b45eb0eed1b4abda56f106233758
suukkii <83240082+suukkii@users.noreply.github.com>
2022-12-19 09:40:50 -0500

    Updating
    Added two more scenes
    working on user input to change scenes

--- src/main/java/IntroScene.java
+++ src/main/java/IntroScene.java
@@ -15,37 +15,35 @@ public class IntroScene extends GameScene {
     public Scene introOne() {
         StackPane sp1 = new StackPane();
 
-        Label monologue1 = new Label();
-        Label monologue2 = new Label();
-        Label monologue3 = new Label();
-        Label monologue4 = new Label();
-        // Label monologue5 = new Label();
-        // Label monologue6 = new Label();
+        Label inTwo1 = new Label();
+        Label inTwo2 = new Label();
+        Label inTwo3 = new Label();
+        Label inTwo4 = new Label();
 
-        monologue1.setText("It's November 10th, we've been in the apocalypse for 3 years now.");
-        monologue2.setText("An evil scientists released the zombie plague which infected over half the city.");
-        monologue3.setText("My sister and I barely survived the first year before she started getting rebellious.");
-        monologue4.setText("One day she went out into the city on our own and got bit...");
-        // monologue5.setText("");
+        inTwo1.setText("It's November 10th, we've been in the apocalypse for 3 years now.");
+        inTwo2.setText("An evil scientists released the zombie plague which infected over half the city.");
+        inTwo3.setText("My sister and I barely survived the first year before she started getting rebellious.");
+        inTwo4.setText("One day she went out into the city on our own and got bit...");
 
-        game.basicPane(sp1);
-        game.styleText(monologue1, 350);
-        game.styleText(monologue2, 390);
-        game.styleText(monologue3, 430);
-        game.styleText(monologue4, 470);
-        // game.smallFont(monologue1);
-        // game.smallFont(monologue2);
-        // game.smallFont(monologue3);
-        // game.smallFont(monologue4);
+        basicPane(sp1);
+        styleText(inTwo1, 350);
+        styleText(inTwo2, 390);
+        styleText(inTwo3, 430);
+        styleText(inTwo4, 470);
+        smallFont(inTwo1);
+        smallFont(inTwo2);
+        smallFont(inTwo3);
+        smallFont(inTwo4);
 
-        sp1.getChildren().add(game.image("\\images\\city.gif", 1000, 300));
-        sp1.getChildren().addAll(monologue1, monologue2, monologue3, monologue4);
+        sp1.getChildren().add(image("\\images\\city.gif", 1000, 300));
+        sp1.getChildren().addAll(inTwo1, inTwo2, inTwo3, inTwo4);
 
-        // sp1.getChildren().add(monologue1);
-        // sp1.getChildren().add(monologue2);
-        // sp1.getChildren().add(monologue2);
-        // sp1.getChildren().add(monologue3);
-        // sp1.getChildren().add(monologue5);
+        //      FOR TIMER 
+        // sp1.getChildren().add(inTwo1);
+        // sp1.getChildren().add(inTwo2);
+        // sp1.getChildren().add(inTwo2);
+        // sp1.getChildren().add(inTwo3);
+        // sp1.getChildren().add(inTwo5);
 
         Scene scene1 = new Scene(sp1, 1000, 800);
 
@@ -55,14 +53,107 @@ public class IntroScene extends GameScene {
 
     public Scene introTwo() {
         StackPane sp2 = new StackPane();
-
-        game.basicPane(sp2);
-
-        sp2.getChildren().add(game.image("\\images\\red city.gif", 1000, 300));
-        sp2.getChildren().add(game.image("\\images\\zombie sister.png", 400, 300));
-
         Scene scene = new Scene(sp2, 1000, 800);
-        return scene;
 
+        basicPane(sp2);
+        
+        Label inTwo1 = new Label();
+        Label inTwo2 = new Label();
+        Label inTwo3 = new Label();
+        Label inTwo4 = new Label();
+
+        inTwo1.setText("I went out that night and spent the whole day looking for her.");
+        inTwo2.setText("When it hit 12 in the morning, I began to make my way back home when suddenly...");
+        inTwo3.setText("I found her!");
+        //long pause
+        inTwo4.setText("and she was a zombie...");
+
+        basicPane(sp2);
+        styleText(inTwo1, 350);
+        styleText(inTwo2, 390);
+        styleText(inTwo3, 430);
+        styleText(inTwo4, 470);
+        smallFont(inTwo1);
+        smallFont(inTwo2);
+        smallFont(inTwo3);
+        smallFont(inTwo4);
+
+        sp2.getChildren().add(image("\\images\\red city.gif", 1000, 300));
+        sp2.getChildren().add(inTwo1);
+        sp2.getChildren().add(inTwo2);
+        sp2.getChildren().add(inTwo3);
+        //LONG PAUSE
+        sp2.getChildren().add(image("\\images\\zombie sister.png", 400, 300));
+        sp2.getChildren().add(inTwo4);
+
+        return scene;
+    }
+
+    public Scene introThree() {
+
+        StackPane sp3 = new StackPane(); 
+        Scene scene = new Scene(sp3, 1000, 800);
+
+        Label inThree1 = new Label();
+        Label inThree2 = new Label();
+        Label inThree3 = new Label();
+        Label inThree4 = new Label();
+        Label inThree5 = new Label(); 
+
+        inThree1.setText("Today, during my daily task of trying to fix the radio,");
+        inThree2.setText("I finally managed to get a connection!");
+        inThree3.setText("I heard static and then...");
+        inThree4.setText("'...come to the safe haven...the stars will lead the way...'");
+        inThree5.setText("So we started to pack our bags.");
+
+        basicPane(sp3);
+        styleText(inThree1, 350);
+        styleText(inThree2, 390);
+        styleText(inThree3, 430);
+        styleText(inThree4, 470);
+        styleText(inThree5, 510);
+        smallFont(inThree1);
+        smallFont(inThree2);
+        smallFont(inThree3);
+        smallFont(inThree4);
+        smallFont(inThree5); 
+
+        sp3.getChildren().add(image("\\images\\radio.jpg", 1000, 300));
+        sp3.getChildren().addAll(inThree1, inThree2, inThree3, inThree4, inThree5);
+
+        return scene;
+    }
+
+    public Scene introFourChoice1() {
+        StackPane sp4 = new StackPane(); 
+        Scene scene = new Scene(sp4, 1000, 800);
+
+        Label title1 = new Label(); 
+        Label title2 = new Label();
+        Label c1 = new Label(); 
+        Label c2 = new Label();
+        Label c3 = new Label(); 
+
+        title1.setText("You can only fit one");
+        title2.setText("more item in you inventory:");
+        c1.setText("1. Med kit");
+        c2.setText("2. Gas");
+        c3.setText("3. Extra Water");
+
+        basicPane(sp4);
+        styleText(title1, 100);
+        styleText(title2, 150);
+        styleText(c1, 300);
+        styleText(c2, 400);
+        styleText(c3, 500);
+        bigFont(title1);
+        bigFont(title2);
+        bigFont(c1);
+        bigFont(c2);
+        bigFont(c3);
+
+        sp4.getChildren().addAll(title1, title2, c1, c2, c3);
+
+        return scene;
     }
 }
